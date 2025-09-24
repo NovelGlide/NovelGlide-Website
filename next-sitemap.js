1deae9f8c3c93f6e184d2bcf76560c28f666418d
Kai <wuvincentck505@gmail.com>
2025-09-25 00:29:29 +0800

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,8 @@
 module.exports = {
   siteUrl: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
   generateRobotsTxt: true,
+  additionalPaths: async (config) => [
+    await config.transform(config, '/locale'),
+    await config.transform(config, '/privacy-policy'),
+  ],
 }
